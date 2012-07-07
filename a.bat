ed99351f2ca5b47591db17f486669d43e7b86dce
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-07-07 13:32:12 +0900

    B4
    v.1
    d.5
    
    D--------------------------
    1. Up to => genres#index ==> Beautified
    
    N--------------------------
    1. New branch => B5_register_genres

--- a.bat
+++ a.bat
@@ -4,6 +4,4 @@ start %SystemRoot%\system32\cmd.exe
 start C:\WORKS\PROGRAMS\FreeMind\Freemind.exe C:\WORKS\WORKSPACES_APTANA\FM_APTANA\NewsReader2\NewsReader2.mm
 gitk
 
-rails s --port 3000
-
 :end
