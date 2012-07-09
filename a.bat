c13940ca7e7fb9a03f7eff1b7db377b811cdd8c2
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-07-10 06:27:20 +0900

    B6
    Just committing

--- a.bat
+++ a.bat
@@ -1,4 +1,4 @@
-start %SystemRoot%\system32\cmd.exe
+REM start %SystemRoot%\system32\cmd.exe
 start %SystemRoot%\system32\cmd.exe
 
 start C:\WORKS\PROGRAMS\FreeMind\Freemind.exe C:\WORKS\WORKSPACES_APTANA\FM_APTANA\NewsReader2\NewsReader2.mm
